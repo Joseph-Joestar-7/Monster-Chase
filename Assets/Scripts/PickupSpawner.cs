111e4c0ccdc0bc0488dd78e8222fcda458156ec2
Ritaban Chaudhuri <inrcr27@gmail.com>
2025-06-13 12:25:38 +0530

    spawning pickups

---
+++ Assets/Scripts/PickupSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] pickupRef;
+    [SerializeField] private Transform[] spawnLoc;
+
+    private int randomIndex;
+    private int randomSpawnIndex;
+    private GameObject spawnedPickup;
+    void Start()
+    {
+        StartCoroutine(SpawnPickups());
+    }
+
+    IEnumerator SpawnPickups()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1, 5));
+
+            randomIndex = Random.Range(0,pickupRef.Length);
+            randomSpawnIndex = Random.Range(0,spawnLoc.Length);
+
+            spawnedPickup = Instantiate(
+                pickupRef[randomIndex],
+                spawnLoc[randomSpawnIndex].position,
+                Quaternion.identity
+            );
+
+
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,7 +24,7 @@ public class Player : MonoBehaviour
     private string ENEMY_TAG = "Enemy";
 
     private bool isGrounded;
-
+    private bool isPhasing=false;
     private void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
